d4114ce3e87bd6a25fb6b18ddf18bee01aad69ff
Cameron Fraser <frasrcl@gmail.com>
2022-06-05 03:08:21 -0700

    more fooling around with rust

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -9,13 +9,18 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 const N: usize = 26;
 
 const A: usize = 'a' as usize;
+const MAX_SPARSE: usize = 8;
 
-/// An attempt at a search trie, optimized for space-efficiency, probably
+/// An attempt at a prefix tree, optimized for space-efficiency, probably
 /// terrible for spatial locality.
 #[derive(Debug)]
 pub enum Trie {
-    Node { terminal: bool, next: Box<[Trie; N]> },
-    Skip { skip: String, then: Box<Trie> },
+    // Dense node variant: represents children by index in fixed-length array
+    Dense { terminal: bool, next: Box<[Trie; N]> },
+    // Sparse node variant: represents children by associative list
+    Sparse { terminal: bool, next: Vec<(u8, Box<Trie>)> },
+    // Nodes which might otherwise have only one child are reduced to strings
+    Skip { terminal: bool, skip: String, then: Box<Trie> },
     Rest { terminal: bool, rest: String },
     End  { terminal: bool },
 }
@@ -24,7 +29,7 @@ impl Trie {
     const EMPTY_END: Self = Self::End { terminal: false };
 
     pub fn empty_root() -> Self {
-        Self::Node { terminal: false, next: Box::new([Self::EMPTY_END; N]) }
+        Self::Dense { terminal: false, next: Box::new([Self::EMPTY_END; N]) }
     }
 
     pub fn add_one(&mut self, word: &str) {
@@ -34,7 +39,7 @@ impl Trie {
 
         loop {
             match node {
-                Self::Node { terminal, next } => {
+                Self::Dense { terminal, next } => {
                     let next_char = word_iter.next();
                     if next_char.is_none() {
                         *terminal = true;
@@ -43,7 +48,10 @@ impl Trie {
                     let (_, &next_idx) = next_char.unwrap();
                     node = &mut next[(next_idx as usize) - A];
                 },
-                Self::Skip { skip, then } => {
+                Self::Sparse { terminal, next } => {
+
+                },
+                Self::Skip { terminal, skip, then } => {
 
                 },
                 Self::Rest { terminal, rest } => {
@@ -60,9 +68,9 @@ impl Trie {
                         return;
                     }
                     let (i, _) = next_char.unwrap();
-                    // let rest = String::with_capacity(word.len() - i + 1);
-                    // rest.push_str(&word[(i-1)..]);
-                    // *node = Self::Rest { terminal: *terminal, rest: rest }
+                    let rest = String::with_capacity(word.len() - i + 1);
+                    rest.push_str(&word[(i-1)..]);
+                    *node = Self::Rest { terminal: *terminal, rest: rest }
                 }
             }
         }
@@ -81,7 +89,7 @@ mod tests {
 
     #[test]
     fn size_of() {
-        assert_eq!(std::mem::size_of::<Trie>(), 32);
+        assert_eq!(std::mem::size_of::<Trie>(), 40);
     }
 
     // #[test]
